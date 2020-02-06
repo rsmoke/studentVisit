2fbe0afd9ee2de08ae20829c7da3f3bc4de37267
Rick Smoke <rsmoke@umich.edu>
2020-02-06 14:38:24 -0500

    add new fields to download file

--- ADMIN/php/stuDetailDownload.php
+++ ADMIN/php/stuDetailDownload.php
@@ -10,11 +10,11 @@ header('Content-Disposition: attachment; filename=StudentDetail.csv');
 $output = fopen('php://output', 'w');
 
 // output the column headings
-fputcsv($output, array('FirstName', 'LastName', 'Email', 'UMid', 'NotAttending', 'Attending', 'AirlineTkt','HotelRes','ShuttleArrive','ShuttleDepart','FacApptMade','WelcomeDinner','RecLunch','DeptLunch','StudentBio', 'Diet Restriction'));
+fputcsv($output, array('FirstName', 'LastName', 'Email', 'UMid', 'NotAttending', 'Attending', 'AirlineTkt','HotelRes','ShuttleArrive','ShuttleDepart','FacApptMade','WelcomeDinner','RecLunch','DeptLunch','StudentBio', 'Diet Restriction', 'Accessibility', 'T-Shirt'));
 
 // fetch the data
 $sqlQuery = <<<SQL
-	SELECT Fname, Lname, email, password, notAttend, attend, airlineTkt, hotel, shuttleArrive, ShuttleDepart, facAppt, welcomeDinner, recLunch, deptLunch, studentBio, dietrestriction
+	SELECT Fname, Lname, email, password, notAttend, attend, airlineTkt, hotel, shuttleArrive, ShuttleDepart, facAppt, welcomeDinner, recLunch, deptLunch, studentBio, dietrestriction, accessibility, shirt
 	FROM tbl_user
 	ORDER by Lname ASC
 
